c69d899f9b94ea8e1cd77bbb826273c1e7c31cdf
Bohdan Hontarenko <tuoefil@gmail.com>
2016-05-03 14:34:03 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const askFor = (prop) => {
+	let res = prompt(prop);
+
+  if (res == null) {
+  	let exit = confirm('stop?');
+
+    return exit ? null : askFor(prop);
+  }
+  if (res == '') return askFor(prop);
+  return res;
+}
+
+const prompty = (props) => {
+	let res = {};
+
+  for (let i = 0; i < props.length; i++) {
+  	let temp = askFor(props[i].label);
+
+    if (!temp) return null;
+  	res[props[i].field] = temp;
+  }
+  return res;
+}
+
+module.exports = prompty;
